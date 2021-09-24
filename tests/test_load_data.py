9ad09baf69b234433821de8ab2da2519a5afd074
RuslanUchan <ruslanuchan97@gmail.com>
2021-09-24 18:09:45 +0700

    SECOND TEST: full_path to file

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,14 +1,24 @@
+import os
 import tempfile
 import unittest
 
-from utils.load_data import load_paths
+from utils.load_data import load_data, load_paths
 
 
 class TestLoadData(unittest.TestCase):
-
 	def test_raise_error_on_data_directory_empty(self):
 		with tempfile.TemporaryDirectory() as tmpdir:
 			self.assertRaises(FileNotFoundError, load_paths, tmpdir)
+	
+	def test_correct_path_for_files_in_data_directory(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			TEST_FILENAME = os.path.join(tmpdir, 'test.txt')
+
+			with open(TEST_FILENAME, 'w') as f:
+				f.write('something')
+
+			one_file_from_dir = load_paths(tmpdir)[0]
+			self.assertEqual(one_file_from_dir, TEST_FILENAME)
 
 if __name__ == '__main__':
 	unittest.main()
--- utils/load_data.py
+++ utils/load_data.py
@@ -3,8 +3,10 @@ import os
 
 def load_paths(DIR_PATH):
 	files = os.listdir(DIR_PATH)
-
+	
 	if len(files) == 0:
 		raise FileNotFoundError
+	
+	files = [os.path.join(DIR_PATH, file_) for file_ in files]
 
 	return files
