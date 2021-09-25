19032a86ef514250989a42b19aa25cf6ab645503
RuslanUchan <ruslanuchan97@gmail.com>
2021-09-25 11:31:46 +0700

    working models

--- crf.py
+++ crf.py
@@ -1,7 +1,17 @@
-from utils.load_data import load_data
+from utils.load_data import load
 from utils.model import model
-from utils.preprocess import preprocess
+from utils.preprocess import create_features
 
-load_data()
-preprocess()
-model()
+DIR_PATH = 'data'
+
+if __name__ == '__main__':
+	data = load(DIR_PATH)
+	Xf, y = create_features(data)
+
+	# labels = set([tag for ners in y for tag in ners])
+
+	# f1_score, report = model(Xf, y, .3, labels)
+	f1_score, report = model(Xf, y, .3)
+
+	print(f'f1_score: {f1_score}')
+	print(f'report:\n{report}')
--- utils/model.py
+++ utils/model.py
@@ -1,4 +1,23 @@
-def model():
-	print('hi from model')
+from sklearn.model_selection import train_test_split
+from sklearn_crfsuite import CRF
+from sklearn_crfsuite.metrics import flat_classification_report, flat_f1_score
 
-	return 2
+
+# def model(Xf, y, test_size, labels):
+def model(Xf, y, test_size):
+	X_train, X_test, y_train, y_test = train_test_split(Xf, y, test_size=test_size)
+
+	crf = CRF(algorithm = 'lbfgs',
+         c1 = 0.1,
+         c2 = 0.1,
+         max_iterations = 100,
+         all_possible_transitions = False)
+
+	crf.fit(X_train, y_train)
+	y_pred = crf.predict(X_test)
+
+	f1_score = flat_f1_score(y_test, y_pred, average = 'weighted')
+	report = flat_classification_report(y_test, y_pred)
+	# report = flat_classification_report(y_test, y_pred, labels=labels)
+
+	return f1_score, report
