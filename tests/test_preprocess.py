1bc6719b12acaa0d6d20398378f31d6c70a337cd
RuslanUchan <ruslanuchan97@gmail.com>
2021-09-25 10:59:54 +0700

    test create labels and features (minimal)

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,12 +1,35 @@
 import unittest
 
-from utils.preprocess import preprocess
+from utils.preprocess import create_features, sent2labels  # , split
 
 
 class TestPreprocess(unittest.TestCase):
-	def test_preprocess(self):
-		x = preprocess()
-		self.assertEqual(x, 2)
+	def setUp(self):
+		self.data = [
+			[('A', 'O'), ('B', 'O'), ('C', 'O')],
+			[('D', 'O'), ('E', 'O'), ('F', 'O')],
+			[('K', 'O'), ('L', 'O'), ('M', 'O')],
+			[('N', 'O'), ('O', 'O'), ('P', 'O')],
+		]
+
+	def test_create_label_after_loading_data(self):
+		example_sentence = self.data[0]
+		expected = ['O', 'O', 'O']
+
+		output = sent2labels(example_sentence)
+
+		self.assertIsInstance(output, list)
+		self.assertEqual(len(output), len(example_sentence))
+		self.assertCountEqual(output, expected)
+	
+	def test_create_features_after_loading_data(self):
+		output_Xf, output_y = create_features(self.data)
+
+		self.assertIsInstance(output_Xf, list)
+		self.assertIsInstance(output_y, list)
+
+		feature = output_Xf[0][0]
+		self.assertIsInstance(feature, dict)
 
 if __name__ == '__main__':
 	unittest.main()
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,4 +1,46 @@
-def preprocess():
-	print('hi from preprocess')
+def word2features(sent, i):
+    word = sent[i][0]
 
-	return 2
+    features = {
+        'bias': 1.0,
+        'word.lower()': word.lower(),
+        'word[-3:]': word[-3:],
+        'word[-2:]': word[-2:],
+        'word.isupper()': word.isupper(),
+        'word.istitle()': word.istitle(),
+        'word.isdigit()': word.isdigit(),
+    }
+
+    if i > 0:
+        word1 = sent[i-1][0]
+        features.update({
+            '-1:word.lower()': word1.lower(),
+            '-1:word.istitle()': word1.istitle(),
+            '-1:word.isupper()': word1.isupper(),
+        })
+    else:
+        features['BOS'] = True
+
+    if i < len(sent)-1:
+        word1 = sent[i+1][0]
+        features.update({
+            '+1:word.lower()': word1.lower(),
+            '+1:word.istitle()': word1.istitle(),
+            '+1:word.isupper()': word1.isupper(),
+        })
+    else:
+        features['EOS'] = True
+
+    return features
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+def sent2labels(sent):
+    return [label for _, label in sent]
+
+def create_features(data):
+    X_features = [sent2features(s) for s in data]
+    y = [sent2labels(s) for s in data]
+
+    return X_features, y
