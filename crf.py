1cdb9394948c79187417b0f9bf5801e732ec275d
RuslanUchan <ruslanuchan97@gmail.com>
2021-09-25 14:16:53 +0700

    adds parser functionalities (test_size and random-seed parameter)

--- crf.py
+++ crf.py
@@ -1,15 +1,28 @@
+import sys
 import warnings
+
+import numpy as np
 
 warnings.filterwarnings('ignore')  # some class not in test_case (zero division)
 								   # crfsuite passing labels to classification_report
 
 from utils.load_data import load
 from utils.model import train_model
+from utils.parser import create_parser
 from utils.preprocess import create_features
 
 DIR_PATH = 'data'
 
 if __name__ == '__main__':
+	parser = create_parser()
+	parser = parser.parse_args(sys.argv[1:])
+
+	test_size = parser.test_size
+	random_seed = parser.random_seed
+
+	if random_seed:
+		np.random.seed(42)
+
 	data = load(DIR_PATH)
 	Xf, y = create_features(data)
 
